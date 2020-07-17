56036e6577bfc07efe3886360977856458834dc7
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-17 16:58:58 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -2,9 +2,8 @@ import requests
 import xml.etree.ElementTree as etree
 
 r = requests.get('https://news.tut.by/rss/all.rss')
-print(r.text)
 root = etree.fromstring(r.text)
 for child in root:
     print(child.tag, child.attrib)
-    for c in child:
-        print("    ", c.tag, c.attrib)
+    for item in child:
+        print("    ", item.tag, item.attrib)
