afc94a91bed907b18e59b33cf19dbebfa0317787
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-17 16:43:23 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -1,4 +1,8 @@
 import requests
+import xml.etree.ElementTree as etree
 
 r = requests.get('https://news.tut.by/rss/all.rss')
 print(r.text)
+tree = etree.parse(r.text)
+root = tree.getroot()
+print(root)
