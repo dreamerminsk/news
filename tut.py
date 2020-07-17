5f21d99ebeef9132d3d4432a289ea994c155feb9
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-17 17:39:56 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -4,12 +4,10 @@ from pymongo import MongoClient
 
 r = requests.get('https://news.tut.by/rss/all.rss')
 root = etree.fromstring(r.text)
-for child in root:
-    print(child.tag, child.attrib)
-    for item in child:
-        print("    ", item.tag, item.attrib)
-        for c in item:
-            print("        ", c.tag, c.attrib)
+for channel in root.findall('channel'):
+    for item in channel.findall('item'):
+        title = country.find('title').text
+        print(title)
             
 client = MongoClient()
 print(client.news)
