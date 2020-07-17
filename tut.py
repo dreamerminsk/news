43ce5228b9f5cca891d26a3ee5d7432c3e5b72b7
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-17 22:13:08 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -2,21 +2,26 @@ import requests
 import xml.etree.ElementTree as etree
 from pymongo import MongoClient
 
+client = MongoClient()
+news = client.news
+articles = news.articles
+
 r = requests.get('https://news.tut.by/rss/all.rss')
 root = etree.fromstring(r.text)
 i = 0
 for channel in root.findall('channel'):
     for item in channel.findall('item'):
         i += 1
-        print(i)
         title = item.find('title').text
-        print(title)
         link = item.find('link').text
         if '?' in link:
             link = link[:link.find('?')]
-        print(link)
         pub = item.find('pubDate').text
-        print(pub)
-            
-client = MongoClient()
+        if !articles.find_one({"link":link}):
+            articles.insert_one({"link":link,"title":title})
+            print(i)
+            print(title)
+            print(link)
+            print(pub)
+
 print(client.news)
