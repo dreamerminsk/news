55a3bf478c1b57a1933d3f6354dbbe95ec4e048e
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-17 20:39:49 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -4,10 +4,12 @@ from pymongo import MongoClient
 
 r = requests.get('https://news.tut.by/rss/all.rss')
 root = etree.fromstring(r.text)
+i = 1
 for channel in root.findall('channel'):
     for item in channel.findall('item'):
+        i += 1
         title = item.find('title').text
-        print(title)
+        print(i, title)
             
 client = MongoClient()
 print(client.news)
