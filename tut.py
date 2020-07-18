3a429f09256bb68332abe822cda94949f5bfbfb2
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-18 20:03:54 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -39,9 +39,9 @@ for item in total:
         print(title.text)
         t = soup.select_one('p.b-article-details time')
         dt = datetime.datetime.strptime(t.get('datetime'), '%Y-%m-%dT%H:%M:%S%z')
-        print('published', dt)
+        print('published: {0}'.format(dt))
         articles.update_one({'_id': item['_id']}, {'$set': {'published': dt}}, upsert=False)
         cc = soup.select_one('span[itemprop="commentCount"]')
-        print('comments', cc.text)
+        print('comments: {0}'.format(cc.text))
         articles.update_one({'_id': item['_id']}, {'$set': {'comments': int(cc.text)}}, upsert=False)
         break
