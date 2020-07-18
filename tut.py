5e527b0128167107fd582b3e2a0f424539bf7603
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-18 16:22:45 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -35,6 +35,6 @@ for item in total:
         p = requests.get(item['link'])
         soup = BeautifulSoup(p.text, "html.parser")
         t = soup.select_one('p.b-article-details time')
-        dt = datetime.datetime.strptime(t.get('datetime'), '%Y-%m-%dT%H:%M:%S %z')
+        dt = datetime.datetime.strptime(t.get('datetime'), '%Y-%m-%dT%H:%M:%S%z')
         print(dt)
         break
