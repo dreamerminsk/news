7ee01a6542fe19705a2dbbc1136ee0b0c7404afb
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-19 12:51:24 +0300

    Update rss.py

--- rss.py
+++ rss.py
@@ -1,1 +1,17 @@
+import requests
+from pymongo import MongoClient
+import pprint
+from bs4 import BeautifulSoup
 
+print = pprint.pprint
+
+client = MongoClient()
+news = client.news
+articles = news.articles
+
+r = requests.get('https://news.tut.by/rss.html')
+soup = BeautifulSoup(r.text, "html.parser")
+feeds = soup.select('li.lists__li a')
+for feed in feeds:
+    print(feed.text)
+    print(feed.get('href'))
