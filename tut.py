520c1126bc933bc6184c3e6463f376a5f4fc28b8
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-19 16:10:54 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -58,5 +58,5 @@ for item in total:
             break
 print('***************************************************')
 print('articles: {}'.format(articles.count_documents({})))
-print('{}'.format(type(articles.command('collStat'))))
+print('{}'.format(type(news.command('dbstats'))))
 
