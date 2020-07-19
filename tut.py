72dc6ac61d5459fe1d6a3abe0ca9b9b7549c22e0
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-19 13:56:23 +0300

    Update tut.py

--- tut.py
+++ tut.py
@@ -58,3 +58,5 @@ for item in total:
             break
 print('***************************************************')
 print('articles: {}'.format(articles.count_documents({})))
+print('{}'.format(articles.stats()))
+
