9a2405feb8c2d66ac42f883d33206d95ccdff9bb
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-19 13:07:32 +0300

    Update rss.py

--- rss.py
+++ rss.py
@@ -9,13 +9,16 @@ client = MongoClient()
 news = client.news
 feeds = news.feeds
 
+feeds.remove({})
+
 r = requests.get('https://news.tut.by/rss.html')
 soup = BeautifulSoup(r.text, "html.parser")
 links = soup.select('li.lists__li a')
 for link in links:
-    print(link.text)
-    print(link.get('href'))
-    feeds.update_one({'link': link.get('href')}, {'$set': {'title': link.text}}, upsert=True)
+    if link.get('href').endswith('.rss'):
+        print(link.text)
+        print(link.get('href'))
+        feeds.update_one({'link': link.get('href')}, {'$set': {'title': link.text}}, upsert=True)
     
 for feed in feeds.find():
     print(feed)
