2ba42216757d8874910ba6327c227c53e48c525d
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-20 20:22:54 +0300

    Update news.py

--- news.py
+++ news.py
@@ -1,5 +1,5 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, PlainTextResponse
 from starlette.routing import Route
 from pymongo import MongoClient
 from datetime import datetime, timedelta
@@ -15,10 +15,13 @@ async def homepage(request):
     client = MongoClient()
     news = client.news
     articles = news.articles
-    article = articles.find({}).sort([("published", -1)]).limit(1)[0]
-    article['_id'] = str(article['_id'])
-    article['published'] = str(article['published'])
-    return JSONResponse(article)
+    arts = articles.find({}).sort([("published", -1)]).limit(16)
+    text = ''
+    for art in arts:
+        text += art['title'] + '\r\n'
+        text += art['published'] + '\r\n'
+        text += '\r\n'
+    return PlainTextResponse(text)
 
 
 app = Starlette(debug=True, routes=[
