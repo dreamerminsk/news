49dd88aa9e20ac81a399485ff199bf1c1bd1e14d
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-20 12:32:56 +0300

    Update news.py

--- news.py
+++ news.py
@@ -15,7 +15,9 @@ async def homepage(request):
     client = MongoClient()
     news = client.news
     articles = news.articles
-    article = articles.find_one({"published": datetime.now().date()})
+    now = datetime.now()
+    nowp1 = datetime.now() + datetime.timedelta(days=1)
+    article = articles.find_one({"published": {"gte": datetime(now.year, now.month, now.day), "lt": datetime(nop1.year, nowp1.month, nowp1.day)}})
     article['_id'] = str(article['_id'])
     article['published'] = str(article['published'])
     return JSONResponse(article)
