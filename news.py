9f6d2366b5c58e81f0304f57f14da4e400f9ea52
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-20 11:19:11 +0300

    Update news.py

--- news.py
+++ news.py
@@ -1,10 +1,20 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from pymongo import MongoClient
+
+
+client = MongoClient()
+news = client.news
+articles = news.articles
+feeds = news.feeds
 
 
 async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+    client = MongoClient()
+    news = client.news
+    articles = news.articles    
+    return JSONResponse(articles.find_one({}))
 
 
 app = Starlette(debug=True, routes=[
