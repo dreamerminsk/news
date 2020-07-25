5b37003507c0ce53eeba8ce6d29e23c8b2ca987f
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-07-25 19:08:58 +0300

    Update feeds.js

--- static/js/feeds.js
+++ static/js/feeds.js
@@ -17,7 +17,7 @@
       let match = document.getElementById(id);
       match.classList.add('bg-success');
       match.classList.add('text-white');
-      let t = document.querySelector(`#${id} .table`);
+      let t = document.querySelector(`div#${id} .table`);
       t.classList.add('table-success');
       t.classList.add('text-white');
     });
