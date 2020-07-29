dc7e716326230a88d2f5f3baa7df2a1833e23a56
Каролина Чурейно <kchureyno@outlook.com>
2020-07-29 13:35:48 +0300

    Update rss.py

--- rss.py
+++ rss.py
@@ -10,27 +10,6 @@ client = MongoClient()
 news = client.news
 feeds = news.feeds
 
-r = requests.get('https://news.tut.by/rss.html')
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.select('li.lists__li a')
-for link in links:
-    if link.get('href').endswith('.rss'):
-        print(link.text)
-        print(link.get('href'))
-        feeds.update_one({'link': link.get('href')}, {
-                         '$set': {'title': link.text, 'ttl': 100}}, upsert=True)
-
-
-r = requests.get('https://www.onliner.by')
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.select('a[href]')
-for link in links:
-    if link.get('href').endswith('feed'):
-        print(link.text)
-        print(link.get('href'))
-        feeds.update_one({'link': link.get('href')}, {
-                         '$set': {'title': link.text, 'ttl': 100}}, upsert=True)
-
 
 feeds.update_one(
     {'link': 'https://www.eurosport.ru/rss.xml'},
