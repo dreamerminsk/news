0644172dc37277233d70196ef8e8ad1a381083cb
Каролина Чурейно <kchureyno@outlook.com>
2020-07-29 10:21:14 +0300

    Update rss.py

--- parsers/rss.py
+++ parsers/rss.py
@@ -6,28 +6,28 @@ import asyncio
 
 async def parse(text):
     soup = BeautifulSoup(text, 'xml')
-    channel  = {}
+    channel = {}
     for channel_node in soup.find_all('channel'):
         channel = await parse_channel(channel_node)
     return channel
 
 
 async def parse_channel(node):
-    channel  = {}
+    channel = {}
     channel['title'] = node.find('title').text
     channel['link'] = node.find('link').text
     channel['description'] = node.find('description').text
     channel['items'] = []
     for item_node in node.find_all('item'):
         channel['items'].append(await parse_item(item_node))
-    return channel 
+    return channel
 
 
 async def parse_item(item):
-    article  = {}
+    article = {}
     article['title'] = item.find('title').text
     article['link'] = item.find('link').text
     if '?' in article['link']:
         article['link'] = article['link'][:article['link'].find('?')]
     article['last_access'] = datetime.now()
-    return article                 
+    return article
