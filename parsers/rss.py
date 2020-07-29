44e3d4a39f0df7be02608e0ee9ad96158b2eaa32
Каролина Чурейно <kchureyno@outlook.com>
2020-07-29 09:47:55 +0300

    Update rss.py

--- parsers/rss.py
+++ parsers/rss.py
@@ -3,20 +3,20 @@ import xml.etree.ElementTree as etree
 from bs4 import BeautifulSoup
 import asyncio
 
+
 async def parse(text):
     soup = BeautifulSoup(text, 'xml')
-    for channel in root.findall('channel'):
+    for channel in soup.findall('channel'):
         for item in channel.findall('item'):
             article = await parse_item(item)
 
 
 async def parse_item(item):
-    title = item.find('title').text
-        link = item.find('link').text
-        if '?' in link:
-            link = link[:link.find('?')]
-        if not articles.find_one({"link": link}):
-            articles.insert_one({"link": link, "title": title})
+    item  = {}
+    item.title = item.find('title').text
+    item.link = item.find('link').text
+    if '?' in link:
+        link = link[:link.find('?')]
     feeds.update_one({'_id': feed['_id']}, {
                      '$set': {'last_access': datetime.now()}}, upsert=False)
     if i > 0:
@@ -31,3 +31,4 @@ async def parse_item(item):
         feeds.update_one({'_id': feed['_id']}, {
             '$set': {'next_access': datetime.now() + timedelta(seconds=1.1 * feed['ttl'])
                      }}, upsert=False)
+    return item                 
