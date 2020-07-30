7441640fa3f6e088fb6e0d82a6d353c6be36d387
Каролина Чурейно <kchureyno@outlook.com>
2020-07-30 18:07:32 +0300

    Update tutusers.py

--- tutusers.py
+++ tutusers.py
@@ -21,9 +21,15 @@ threads_url = 'https://talks.by/forumdisplay.php?f=45'
 threads_page = get_text(threads_url)
 if threads_page:
     soup = BeautifulSoup(threads_page, 'html.parser')
-    
+    ref_nodes = soup.select('a[href]')
+    if ref_nodes:
+        for ref_node in ref_nodes:
+            query = parse.urlsplit(ref_node.get('href')).query
+            params = parse.parse_qs(query)
+            if 't' in params:
+                print(params['t'][0])
 
-text = get_text('https://talks.by/showthread.php?t=14464110&page=5')
+text = get_text('https://talks.by/showthread.php?t=14463681&page=2')
 if text:
     soup = BeautifulSoup(text, 'html.parser')
     user_nodes = soup.select('div.row-user a.username')
