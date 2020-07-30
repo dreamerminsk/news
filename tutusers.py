8baa489777ba34d007160be115b68e4b30463979
Каролина Чурейно <kchureyno@outlook.com>
2020-07-30 13:43:02 +0300

    Update tutusers.py

--- tutusers.py
+++ tutusers.py
@@ -8,6 +8,7 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 from web import get_text
+from urllib import parse
 
 
 #print = pprint
@@ -22,5 +23,8 @@ if text:
     user_nodes = soup.select('div.row-user a.username')
     if user_nodes:
         for user_node in user_nodes:
+            query = parse.urlsplit(user_node.get('href')).query
+            params = parse.parse_qs(query)
             print(user_node.text)
             print(user_node.get('href'))
+            print(user_node.params['u'][0])
