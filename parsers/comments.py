aa02b23ec2ef868ef94311895f11578fa405028d
Каролина Чурейно <kchureyno@outlook.com>
2020-08-01 13:48:43 +0300

    Update comments.py

--- parsers/comments.py
+++ parsers/comments.py
@@ -3,3 +3,7 @@
 ### td.themaCommentLeft
 #### div.rowUser id > a.postuseravatar > img, a.username
 ### td.themaCommentRight
+#     a.name
+#     div.row-content-date
+#       Добавлено: Сегодня 08:29 из Беларуси
+#       Добавлено: 19.07.20 12:39 из Беларуси
