735dd956a4b8db65b4c40f6623d273774eb95bf3
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-08-04 16:46:33 +0300

    Update test.py

--- wikidata/test.py
+++ wikidata/test.py
@@ -1,11 +1,18 @@
-from qwikidata.sparql  import return_sparql_query_results
+from qwikidata.sparql import (get_subclasses_of_item,
+                              return_sparql_query_results)
 from qwikidata.entity import WikidataItem, WikidataLexeme, WikidataProperty
 from qwikidata.linked_data_interface import get_entity_dict_from_api
 
 
-query_string = """SELECT (COUNT(?item) AS ?count)
-WHERE {
-        ?item wdt:P31/wdt:P279* wd:Q5 .
+query_string = """SELECT ?book ?title ?illustratorLabel ?publisherLabel ?published
+WHERE
+{
+  ?book wdt:P50 wd:Q35610.
+  OPTIONAL { ?book wdt:P1476 ?title. }
+  OPTIONAL { ?book wdt:P110 ?illustrator. }
+  OPTIONAL { ?book wdt:P123 ?publisher. }
+  OPTIONAL { ?book wdt:P577 ?published. }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
 }"""
 
 res = return_sparql_query_results(query_string)
@@ -13,7 +20,6 @@ print(res)
 for row in res["results"]["bindings"]:
    print(row)
    
-Q_DOUGLAS_ADAMS = "Q42"
-q42_dict = get_entity_dict_from_api(Q_DOUGLAS_ADAMS)
-q42 = WikidataItem(q42_dict)
-print(q42)
+Q_RIVER = "Q4022"
+subclasses_of_river = get_subclasses_of_item(Q_RIVER)
+print(subclasses_of_river)
