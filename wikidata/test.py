abd38b7bf91053942fc84b4d2bac3bb3058cb1fd
Каролина Чурейно <kchureyno@outlook.com>
2020-08-04 12:00:49 +0300

    Update test.py

--- wikidata/test.py
+++ wikidata/test.py
@@ -3,18 +3,17 @@ from qwikidata.entity import WikidataItem, WikidataLexeme, WikidataProperty
 from qwikidata.linked_data_interface import get_entity_dict_from_api
 
 
-query_string = """SELECT ?book ?title ?illustratorLabel ?publisherLabel ?published
-WHERE
-{
-  ?book wdt:P50 wd:Q35610;
-        wdt:P1476 ?title;
-        wdt:P110 ?illustrator;
-        wdt:P123 ?publisher;
-        wdt:P577 ?published.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
+query_string = """SELECT (COUNT(?item) AS ?count)
+WHERE {
+        ?item wdt:P31/wdt:P279* wd:Q5 .
 }"""
 
 res = return_sparql_query_results(query_string)
 print(res)
 for row in res["results"]["bindings"]:
    print(row)
+   
+Q_DOUGLAS_ADAMS = "Q42"
+q42_dict = get_entity_dict_from_api(Q_DOUGLAS_ADAMS)
+q42 = WikidataItem(q42_dict)
+print(q42)
