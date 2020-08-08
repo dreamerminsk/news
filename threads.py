faca2173788f104e68254e2ff6a19abbe267df64
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-08-08 12:08:05 +0300

    Update threads.py

--- threads.py
+++ threads.py
@@ -37,29 +37,9 @@ while len(turls) > 0:
                 query = parse.urlsplit(ref_node.get('href')).query
                 params = parse.parse_qs(query)
                 if 't' in params:
+                    print('{}. {}'.format(params['t'][0], ref_node.text))
                     urls.add(
                         'https://talks.by/showthread.php?t={}'.format(params['t'][0]))
     print(len(urls))
 
-while len(urls) > 0:
-    url = urls.pop()
-    print(url)
-    text = get_text(url)
-    if text:
-        soup = BeautifulSoup(text, 'html.parser')
-        ref_nodes = soup.select('a[href]')
-        if ref_nodes:
-            for ref_node in ref_nodes:
-                if '>>' in ref_node.text:
-                    urls.add('https://talks.by/{}'.format(ref_node.get('href')))
-        user_nodes = soup.select('div.row-user a.username')
-        if user_nodes:
-            for user_node in user_nodes:
-                query = parse.urlsplit(user_node.get('href')).query
-                params = parse.parse_qs(query)
-                op_result = users.update_one({'u': params['u'][0]}, {
-                    '$set': {'name': user_node.text}}, upsert=True)
-                #pp.pprint('{}, {}, {}'.format(op_result.matched_count, op_result.modified_count, op_result.upserted_id))
-                if op_result.upserted_id:
-                    print('\t{} - {}'.format(params['u'][0], user_node.text))
 print(users.count_documents({}))
