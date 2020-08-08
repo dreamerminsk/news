887202dde135a7db5ce1f4346998d926920dd170
Каролина Чурейно <kchureyno@outlook.com>
2020-08-08 17:18:21 +0300

    Update threads.py

--- threads.py
+++ threads.py
@@ -2,14 +2,13 @@ import asyncio
 import pprint
 import xml.etree.ElementTree as etree
 from datetime import datetime, timedelta
+from urllib import parse
 
 from bs4 import BeautifulSoup
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 from web import get_text
-from urllib import parse
-
 
 #print = pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -37,11 +36,14 @@ while len(turls) > 0:
             for ref_node in ref_nodes:
                 query = parse.urlsplit(ref_node.get('href')).query
                 params = parse.parse_qs(query)
-                if 't' in params:                    
-                    thread_url = 'https://talks.by/showthread.php?t={}'.format(params['t'][0])
+                if 't' in params:
+                    thread_url = 'https://talks.by/showthread.php?t={}'.format(
+                        params['t'][0])
                     if thread_url not in urls:
                         print('{}. {}'.format(params['t'][0], ref_node.text))
                         urls.add(thread_url)
+                        threads.update_one({'thread_id': params['t'][0]}, {
+                                           '$set': {'title': ref_node.text}}, upsert=True)
     print(len(urls))
 
 print(threads.count_documents({}))
