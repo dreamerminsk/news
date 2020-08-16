cd44181f92b980b82a7148ca36d0f94a557fa671
Каролина Чурейно <kchureyno@outlook.com>
2020-08-16 09:48:31 +0300

    Update humans.py

---
+++ rels/humans.py
@@ -0,0 +1,25 @@
+import asyncio
+import pprint
+from datetime import datetime, timedelta
+
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import (JSONResponse, PlainTextResponse,
+                                 RedirectResponse)
+
+
+class HumansEndpoint(HTTPEndpoint):
+    async def get(self, request):
+        name = request.path_params['name']
+        task = news.tasks.find_one({"name": name})
+        if task is not None:
+            task['_id'] = str(task['_id'])
+            task['elapsed'] = str(datetime.now() - task['start'])
+            task['start'] = str(task['start'])
+            task['total'] = articles.count_documents({})
+        else:
+            task = {
+            }
+        return JSONResponse(task)
