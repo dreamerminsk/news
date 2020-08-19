cab711be35a3b6de04c1ab0ee25193387aa128cd
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-08-19 11:11:26 +0300

    Update countries.py

--- rels/countries.py
+++ rels/countries.py
@@ -15,6 +15,21 @@ rels = client.rels
 
 class CountriesEndpoint(HTTPEndpoint):
     async def get(self, request):
+        task = rels.countries.find({})
+        if task is not None:
+            task['_id'] = str(task['_id'])
+        else:
+            task = {
+            }
+        return JSONResponse(task)
+      
+    async def post(self, request):
+        pass
+      
+      
+      
+class CountryEndpoint(HTTPEndpoint):
+    async def get(self, request):
         wdid = request.path_params['wikidataid']
         task = rels.countries.find_one({'WikiDataID': wdid})
         if task is not None:
@@ -23,3 +38,6 @@ class CountriesEndpoint(HTTPEndpoint):
             task = {
             }
         return JSONResponse(task)
+      
+    async def put(self, request):
+        pass
