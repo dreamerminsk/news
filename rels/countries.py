76b1d78614a3e5f001b191bd9e43be85a2cd9599
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-08-19 10:59:42 +0300

    Create countries.py

---
+++ rels/countries.py
@@ -0,0 +1,25 @@
+import asyncio
+import pprint
+from datetime import datetime, timedelta
+
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import (JSONResponse, PlainTextResponse,
+                                 RedirectResponse)
+
+client = MongoClient()
+rels = client.rels
+
+
+class CountriesEndpoint(HTTPEndpoint):
+    async def get(self, request):
+        wdid = request.path_params['wikidataid']
+        task = rels.countries.find_one({'WikiDataID': wdid})
+        if task is not None:
+            task['_id'] = str(task['_id'])
+        else:
+            task = {
+            }
+        return JSONResponse(task)
