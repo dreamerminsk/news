ea52a794e52777febccd3580bb922c9484fdb556
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-08-29 20:15:50 +0300

    Update logging.py

--- middleware/logging.py
+++ middleware/logging.py
@@ -12,10 +12,16 @@ client = MongoClient()
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        client.stats.hosts.update_one({'host': request.client.host}, {
-                                      '$inc': {'requests': 1}}, upsert=True)
-        client.stats.hosts.update_one({'host': request.client.host}, {
-                                      '$set': {'last': datetime.now()}}, upsert=True)
-        client.stats.hosts.update_one({'host': request.client.host}, {
-                                      '$set': {'last_path': request.url.path}}, upsert=True)
+        client.stats.hosts.update_one({ 'host' : request.client.host }, {
+                                      '$inc' : { 'requests': 1 } }, upsert=True)
+        client.stats.hosts.update_one(
+            { 'host': request.client.host }, 
+            { '$inc': { 'last-req-count' : 1 }, 
+                '$push' : { 'last-requests' : { 'datetime' : 'datetime.now(), 'path' : request.url.path } } 
+            }
+        )
+        client.stats.hosts.update_one(
+            { 'host': request.client.host, 'last-req-count' : { '$gt': 3 } }, 
+            { '$inc': { 'last-req-count' : -1 }, '$pop' : { 'last-requests' : -1 } }
+        )
         return response
