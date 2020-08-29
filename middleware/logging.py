710af3f6e1dd9f5813ba8b6512b6bf23df54df3e
Каролина Чурейно <kchureyno@outlook.com>
2020-08-29 20:43:10 +0300

    Update logging.py

--- middleware/logging.py
+++ middleware/logging.py
@@ -12,14 +12,15 @@ client = MongoClient()
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        client.stats.hosts.update_one({ 'host' : request.client.host }, {
-                                      '$inc' : { 'requests': 1 } }, upsert=True)
+        client.stats.hosts.update_one({'host': request.client.host}, {
+                                      '$inc': {'requests': 1}}, upsert=True)
         client.stats.hosts.update_one(
-            { 'host': request.client.host }, 
-            { '$inc': { 'last-req-count' : 1 }, 
-                '$push' : { 'last-requests' : { 'datetime' : 'datetime.now(), 'path' : request.url.path } } 
+            {'host': request.client.host},
+            {
+                '$inc': {'last-req-count': 1},
+                '$push': {'last-requests': {'datetime': datetime.now(), 'path': request.url.path}}
             })
         client.stats.hosts.update_one(
-            { 'host': request.client.host, 'last-req-count' : { '$gt': 3 } }, 
-            { '$inc': { 'last-req-count' : -1 }, '$pop' : { 'last-requests' : -1 } })
+            {'host': request.client.host, 'last-req-count': {'$gt': 3}},
+            {'$inc': {'last-req-count': -1}, '$pop': {'last-requests': -1}})
         return response
