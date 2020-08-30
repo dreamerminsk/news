a4b6f37a008c0593efa7cc8c28f7d6ea4bd72a57
Каролина Чурейно <kchureyno@outlook.com>
2020-08-30 18:11:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
 
 import uvicorn
-
-from chat.events.open_redis_cache import open_redis_cache
-from chat.urls import setup_urls
 from core.handlers.app import StarletteCustom
 
-
 app = StarletteCustom(template_directory='templates')
-app.add_event_handler('startup', open_redis_cache(app))
-setup_urls(app)
+#app.add_event_handler('startup', open_redis_cache(app))
+# setup_urls(app)
 
 
 if __name__ == "__main__":
