5750d47742c32d17f5871b942b9c35aa67e24823
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-11-03 10:46:36 +0300

    Update nnm.py

--- workers/nnm.py
+++ workers/nnm.py
@@ -7,16 +7,16 @@ n=1
 for t in soup.select('table.pline'):
     for link in t.select('a[href]'):
         if link['href'].startswith('magnet'):
-            pcatHead = t.select_one('td.pcatHead')
-            print(n, pcatHead.text)
+            pcatHead = t.select_one('td.pcatHead a.pgenmed')
+            print(n, pcatHead['title'])
             print('\t', link['href'])
-            if ' - ' in pcatHead.text:
-                ad = pcatHead.text.find(' - ')
-                td = pcatHead.text.find(' (', ad)
-                td = td if td > -1 else pcatHead.text.find(' [', ad)
-                print('\t', pcatHead.text[0 : ad].strip())
-                print('\t', pcatHead.text[ad + 3 : td].strip())
-                gs = pcatHead.text[pcatHead.text.find('<')+1 : pcatHead.text.find('>')].strip()
+            if ' - ' in pcatHead['title']:
+                ad = pcatHead['title'].find(' - ')
+                td = pcatHead['title'].find(' (', ad)
+                td = td if td > -1 else pcatHead['title'].find(' [', ad)
+                print('\t', pcatHead['title'][0 : ad].strip())
+                print('\t', pcatHead['title'][ad + 3 : td].strip())
+                gs = pcatHead['title'][pcatHead.text.find('<')+1 : pcatHead['title'].find('>')].strip()
                 print('\t', gs.strip())
             n=n+1
             
