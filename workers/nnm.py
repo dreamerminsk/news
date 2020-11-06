9d4d7a433640fa04d98c4824a25a9b0a7d8fddfe
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-11-06 12:17:57 +0300

    Update nnm.py

--- workers/nnm.py
+++ workers/nnm.py
@@ -1,22 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://nnmclub.to/forum/portal.php?c=12&sid=3c44b5b5baf94f59642bd0a373db4884')
+r = requests.get('http://nnmclub.to/forum/portal.php?c=12')
 soup = BeautifulSoup(r.text, features='html.parser')
-n=1
+n = 1
 for t in soup.select('table.pline'):
     for link in t.select('a[href]'):
         if link['href'].startswith('magnet'):
             pcatHead = t.select_one('td.pcatHead a.pgenmed')
-            print(n, pcatHead['title'])
-            print('\t', link['href'])
+            print ('')
+            print (n, pcatHead['title'])
+            print ('\tTOPIC\t', pcatHead['href'][16:])
+            print ('\tHASH\t', link['href'][20:])
             if ' - ' in pcatHead['title']:
                 ad = pcatHead['title'].find(' - ')
                 td = pcatHead['title'].find(' (', ad)
-                td = td if td > -1 else pcatHead['title'].find(' [', ad)
-                print('\t', pcatHead['title'][0 : ad].strip())
-                print('\t', pcatHead['title'][ad + 3 : td].strip())
-                gs = pcatHead['title'][pcatHead.text.find('<')+1 : pcatHead['title'].find('>')].strip()
-                print('\t', gs.strip())
-            n=n+1
-            
+                td = (td if td > -1 else pcatHead['title'].find(' [', ad))
+                print ('\tARTIST\t', (pcatHead['title'])[0:ad].strip())
+                print ('\tTITLE\t', (pcatHead['title'])[ad + 3:td].strip())
+                gs = (pcatHead['title'])[pcatHead.text.find('<')
+                    + 1:pcatHead['title'].find('>')].strip()
+                print ('\tGENRES\t', gs.strip())
+            n = n + 1
