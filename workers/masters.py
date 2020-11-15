cb6990798c6e20fa7069faedc2397389ac615aa1
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-11-15 11:27:34 +0300

    Update masters.py

--- workers/masters.py
+++ workers/masters.py
@@ -2,9 +2,20 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+import time
+
+
+def wiki(page):
+    print(page)
+    time.sleep(4000)
+    url = 'https://en.wikipedia.org' + page
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, features='html.parser')
+
 
 r = requests.get('https://en.wikipedia.org/wiki/Tennis_Masters_Series_records_and_statistics')
 soup = BeautifulSoup(r.text, features='html.parser')
 for link in soup.select('a[href]'):
     if '*' in link.text:
         print (link.text, link['href'])
+        wiki(link['href'])
