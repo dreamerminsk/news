c06207048b1628e19f2be374f9b6153eeb7134cb
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-11-19 12:20:04 +0300

    Update masters.py

--- workers/masters.py
+++ workers/masters.py
@@ -15,7 +15,7 @@ def wiki(page):
         print ('\t', link.text, '\n\r')
     for link in soup.select('#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr'):
         for scope in link.select('th'):
-            print ('\t', scope.text, '\n\r')
+            print ('\tSCOPE', scope.text, '\n\r')
     for link in soup.select('#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(3) > td > a'):
         print ('\t', link['title'], '\n\r')
     for link in soup.select('#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(4) > td > a'):
