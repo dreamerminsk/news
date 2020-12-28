0aa12ef98b01a82d3ba8e8d24e0e7f0f94f14b0e
Micah Teeuws <tutmirage@gmail.com>
2020-12-28 21:34:39 +0300

    Update news.py

--- news.py
+++ news.py
@@ -132,6 +132,8 @@ async def start_job():
     print('{}. {}'.format(datetime.now(), count))
     news.tasks.update_one({'name': 'feeds'}, {
         '$set': {'start': datetime.now(), 'feeds': 0, 'articles': count}}, upsert=True)
+    feeds.update_one({'link': 'https://meduza.io/rss/all'}, {
+                '$set': {'last_access': datetime.now(), 'next_access': datetime.now(), 'ttl': 1000}}, upsert=False)
     # client.rels.categories.remove({})
     # client.rels.categories.insert_one(
     #     {'labels': {'en': 'Category:Peter the Great'}})
