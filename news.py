4a53c2157668aa2596c7c8508dc02a57ce93ad4e
Micah Teeuws <tutmirage@gmail.com>
2020-12-29 12:31:03 +0300

    Update news.py

--- news.py
+++ news.py
@@ -173,7 +173,7 @@ async def queue_cat():
         
         
 async def queue_ibu():
-    links = await get_links('ru', 'Кубок мира по биатлону 2020/2021')
+    links = await get_links('ru', 'Кубок мира по биатлону 2019/2020')
     for link in links['links']:
         if ', ' not in link:
             continue
