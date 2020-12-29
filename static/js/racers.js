bd3cec1f9204f35921f591b0f1aa4f4fc1f5cf9b
Micah Teeuws <tutmirage@gmail.com>
2020-12-29 11:54:50 +0300

    Update racers.js

--- static/js/racers.js
+++ static/js/racers.js
@@ -1,61 +1,12 @@
-async function updateFeeds() {
-  let response = await fetch('/api/tasks/feeds');
-  let commits = await response.json();
-  document.getElementById("task-start").textContent = commits.start.toLocaleString();
-  document.getElementById("task-elapsed").textContent = commits.elapsed;
-  document.getElementById("task-feeds").textContent = `${commits.feeds}`;
-  document.getElementById("task-articles").textContent = `${commits.total - commits.articles}`;
-  document.querySelectorAll('div.card').forEach(div => {
-    if (div.classList.contains('bg-success')) {
-      div.classList.remove('bg-success');
-      div.classList.remove('text-white');
-    }
-  });
-  document.querySelectorAll('.table').forEach(div => {
-    if (div.classList.contains('text-white')) {
-      div.classList.remove('text-white');
-    };
-  });
-  let res = await fetch(`/api/feeds/latest`);
-  let json = await res.json();
-  json.feeds.forEach(feed => {
-    try {
-      let match = document.getElementById(feed['_id']);
-      match.classList.add('bg-success');
-      match.classList.add('text-white');
-      let t = match.querySelector(`.table`);
-      t.classList.add('text-white');
-      let la = document.getElementById(`la-${feed['_id']}`);
-      la.textContent = feed['last_access'];
-      let na = document.getElementById(`na-${feed['_id']}`);
-      na.textContent = feed['next_access'];
-      let ttl = document.getElementById(`ttl-${feed['_id']}`);
-      ttl.textContent = feed['ttlf'];
-    } catch (e) {
-      document.getElementById("feeds").textContent += `${e}`;
-    }
-  });
-}
-
-
 async function updateRacers() {
   let response = await fetch('/api/ibu/racers');
   let json = await response.json();
+  document.getElementById("racers").innerHTML = '';
   json.racers.forEach(racer => {
     try {
-      let match = document.getElementById(feed['_id']);
-      match.classList.add('bg-success');
-      match.classList.add('text-white');
-      let t = match.querySelector(`.table`);
-      t.classList.add('text-white');
-      let la = document.getElementById(`la-${feed['_id']}`);
-      la.textContent = feed['last_access'];
-      let na = document.getElementById(`na-${feed['_id']}`);
-      na.textContent = feed['next_access'];
-      let ttl = document.getElementById(`ttl-${feed['_id']}`);
-      ttl.textContent = feed['ttlf'];
+      document.getElementById("racers").innerHTML += `<div class='container'>racer.wiki.ru</div>`;
     } catch (e) {
-      document.getElementById("feeds").textContent += `${e}`;
+      document.getElementById("racers").textContent += `${e}`;
     }
   });
 }
