c06daeebd0d3b9cc62175679114e8b4a2304f6a2
Micah Teeuws <tutmirage@gmail.com>
2020-12-29 12:41:59 +0300

    Update racers.js

--- static/js/racers.js
+++ static/js/racers.js
@@ -5,6 +5,14 @@ async function updateRacers() {
   json.racers.forEach(racer => {
     try {
       document.getElementById("racers").innerHTML += `<div class="card"><div class="card-body">${racer.wiki.ru}</div></div>`;
+      `<div class="card border-dark mb-3">
+         <div class="card-header">${racer.wiki.ru}</div>
+         <div class="card-body text-dark">
+           <h5 class="card-title">${racer.wiki.ru}</h5>
+           <p class="card-text"></p>
+           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+         </div>
+       </div>`;
     } catch (e) {
       document.getElementById("racers").textContent += `${e}`;
     }
