5f642b8a561a9b685086bf3bb5ec1367016e566c
Micah Teeuws <tutmirage@gmail.com>
2020-12-30 23:31:05 +0300

    Update news.py

--- news.py
+++ news.py
@@ -22,7 +22,7 @@ from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from workers.web import get_text
-from workers.wiki import get_category, get_links
+from workers.wiki import get_category, get_links, get_info
 
 #print = pprint.pprint
 
