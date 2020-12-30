1f5c0d93b915639ed762c0671f6bf0796b348faf
Micah Teeuws <tutmirage@gmail.com>
2020-12-30 13:20:59 +0300

    Update racers.js

--- static/js/racers.js
+++ static/js/racers.js
@@ -1,10 +1,21 @@
+let racers = [];
+let selectedTd;
+
 async function updateRacers() {
   let response = await fetch('/api/ibu/racers');
   let json = await response.json();
-  document.getElementById("racers").innerHTML = '';
-  json.racers.forEach(racer => {
-    try {
-      document.getElementById("racers").innerHTML += 
+  if (json.racers) {
+    racers.clear();
+    racers.append(json.racers);
+  }
+}
+
+function filter() {
+  if (!selectedTd) return;
+  const node = document.getElementById("racers");
+  node.innerHTML = '';
+  racers.filter(racer => racer.startsWith(selectedTd.title)).forEach(racer => {
+      node.innerHTML += 
       `<div class="card border-dark mb-3">
          <div class="card-header">${racer.wiki.ru}</div>
          <div class="card-body text-dark">
@@ -13,19 +24,16 @@ async function updateRacers() {
            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
          </div>
        </div>`;
-    } catch (e) {
-      document.getElementById("racers").textContent += `${e}`;
-    }
   });
 }
 
-let selectedTd;
 
 function highlight(td) {
   if (selectedTd) {
     selectedTd.classList.remove('table-success');
   }
   selectedTd = td;
+  filter();
   selectedTd.classList.add('table-success');
 }
 
@@ -45,7 +53,6 @@ async function initLetters() {
 
 
 document.addEventListener('DOMContentLoaded', function(event) {
-    alert('Страница загружена');
     initLetters();
     updateRacers();
 });
