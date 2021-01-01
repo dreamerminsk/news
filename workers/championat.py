24a62d5c54f05c32ba18826f2df12f743a89b602
Micah Teeuws <tutmirage@gmail.com>
2021-01-01 19:20:48 +0300

    Update championat.py

--- workers/championat.py
+++ workers/championat.py
@@ -1,6 +1,3 @@
-https://www.championat.com/
-
-
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
@@ -8,16 +5,13 @@ from workers.web import get_text
 
 
 async def get_category(title):
-    text = get_text('https://en.wikipedia.org/wiki/{}'.format(title))
+    text = get_text('https://www.championat.com/')
     if text is None:
         return {'wikidataid': None, 'categories': []}
     category = {'wikidataid': 'None', 'categories': []}
     if text:
         soup = BeautifulSoup(text, 'html.parser')
-        wdi_node = soup.select_one('li#t-wikibase a[href]')
-        if wdi_node:
-            category['wikidataid'] = wdi_node.get('href').split('/')[-1]
-        cat_nodes = soup.select('div#mw-normal-catlinks ul li a[title]')
+        cat_nodes = soup.select('a[href]')
         if cat_nodes:
             for cat_node in cat_nodes:
                 category['categories'].append(cat_node.get('title'))
