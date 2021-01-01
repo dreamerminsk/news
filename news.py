6eb0f07bf9d574b5407e58ba20fca6ea889a2680
Micah Teeuws <tutmirage@gmail.com>
2021-01-01 20:36:39 +0300

    Update news.py

--- news.py
+++ news.py
@@ -175,15 +175,18 @@ async def queue_cat():
         
 async def queue_ibu():
     await asyncio.sleep(4)
-    links = await get_links('ru', 'Общий зачёт Кубка мира по биатлону')
+    total = 0
+    links = await get_links('ru', 'Чемпионат мира по биатлону 2020 — спринт (женщины)')
     for link in links['links']:
         if ', ' not in link:
             continue
         found = client.ibustats.racers.find_one(
             {'wiki.ru': link})
         if found is None:
+            total += 1
             client.ibustats.racers.insert_one(
                 {'wiki': {'ru': link}})
+    print('queue_ibu(): {}'.format(total))
     await asyncio.sleep(32)
     
     
