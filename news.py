c90f71dfcf0a6e1f83b738fbd3ecefe4187ed1db
Micah Teeuws <tutmirage@gmail.com>
2021-01-05 22:26:48 +0300

    Update news.py

--- news.py
+++ news.py
@@ -151,38 +151,6 @@ async def start_job():
              loop.create_task(queue_wiki_info())]
 
 
-async def queue_ibu2():
-    await asyncio.sleep(4)
-    total = 0
-    links = await get_pages('ru', 'Категория:Биатлонисты по алфавиту')
-    for link in links:
-        found = client.ibustats.racers.find_one(
-            {'wiki.ru': link})
-        if found is None:
-            total += 1
-            client.ibustats.racers.insert_one(
-                {'wiki': {'ru': link}})
-    print('queue_ibu2(): {}'.format(total))
-    await asyncio.sleep(32)
-
-
-async def queue_ibu():
-    await asyncio.sleep(4)
-    total = 0
-    links = await get_links('ru', 'Призёры чемпионатов мира по биатлону (мужчины)')
-    for link in links['links']:
-        if ', ' not in link:
-            continue
-        found = client.ibustats.racers.find_one(
-            {'wiki.ru': link})
-        if found is None:
-            total += 1
-            client.ibustats.racers.insert_one(
-                {'wiki': {'ru': link}})
-    print('queue_ibu(): {}'.format(total))
-    await asyncio.sleep(32)
-
-
 async def queue_wiki_info():
     racers = client.ibustats.racers.find({})
     wikis = []
