35069effada052ee57e86b75563109f8f1c25469
Каролина Чурейно <kchureyno@outlook.com>
2021-01-10 13:08:56 +0300

    Update news.py

--- news.py
+++ news.py
@@ -29,8 +29,6 @@ from workers.wiki import get_category, get_info, get_links, get_pages
 templates = Jinja2Templates(directory='templates')
 
 client = MongoClient()
-client.rels.categories.remove({})
-# client.stats.hosts.remove({})
 news = client.news
 feeds = news.feeds
 articles = news.articles
@@ -148,46 +146,9 @@ async def start_job():
     loop = asyncio.get_event_loop()
     tasks = [loop.create_task(queue_feeds(q)),
              loop.create_task(process_feeds(q)),
-             loop.create_task(process_tournaments()),
              loop.create_task(queue_wiki_info())]
 
 
-async def process_tournaments():
-    await asyncio.sleep(2)
-    text = get_text('https://www.championat.com/biathlon/_biathlonworldcup.html')
-    print('process_tournaments')
-    if text:
-        soup = BeautifulSoup(text, 'html.parser')
-        wdi_nodes = soup.select('script.js-entity-header-select-data')
-        client.ibustats.seasons.update_one({'cc_id': 597}, {
-            '$set': {'title': '2020/2021'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 551}, {
-            '$set': {'title': '2019/2020'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 497}, {
-            '$set': {'title': '2018/2019'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 450}, {
-            '$set': {'title': '2017/2018'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 402}, {
-            '$set': {'title': '2016/2017'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 334}, {
-            '$set': {'title': '2015/2016'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 283}, {
-            '$set': {'title': '2014/2015'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 172}, {
-            '$set': {'title': '2013/2014'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 83}, {
-            '$set': {'title': '2012/2013'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 55}, {
-            '$set': {'title': '2011/2012'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 34}, {
-            '$set': {'title': '2010/2011'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 15}, {
-            '$set': {'title': '2009/2010'}}, upsert=True)
-        client.ibustats.seasons.update_one({'cc_id': 2}, {
-            '$set': {'title': '2008/2009'}}, upsert=True)
-    await asyncio.sleep(2)
-    
-    
 async def queue_wiki_info():
     racers = client.ibustats.racers.find({})
     wikis = []
