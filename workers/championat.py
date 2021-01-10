d3bf59a45fde876b2ee48084129b15ab2f0c9b60
Micah Teeuws <tutmirage@gmail.com>
2021-01-10 13:02:10 +0300

    Update championat.py

--- workers/championat.py
+++ workers/championat.py
@@ -4,77 +4,34 @@ from datetime import datetime, timedelta
 from workers.web import get_text
 
 
-async def get_category(title):
-    text = get_text('https://www.championat.com/')
-    if text is None:
-        return {'wikidataid': None, 'categories': []}
-    category = {'wikidataid': 'None', 'categories': []}
+async def process_tournaments():
+    text = get_text('https://www.championat.com/biathlon/_biathlonworldcup.html')
     if text:
         soup = BeautifulSoup(text, 'html.parser')
-        cat_nodes = soup.select('a[href]')
-        if cat_nodes:
-            for cat_node in cat_nodes:
-                category['categories'].append(cat_node.get('title'))
-    return category
-
-
-
-
-def get_country_info(soup):
-    countries = []
-    nodes = soup.select("span[data-wikidata-property-id='P27'] a[title]")
-    if nodes:
-        for node in nodes:
-            if 'Флаг' in node.get('title'):
-                continue
-            if node.get('title') in countries:
-                continue
-            countries.append(node.get('title'))
-    return countries
-
-def get_name_info(soup):
-    name = None
-    nodes = soup.select("div.ts_Спортсмен_имя div.label")
-    if nodes:
-        for node in nodes:
-            name = node.text
-    return name
-
-def get_bday_info(soup):
-    name = None
-    nodes = soup.select("span.bday")
-    if nodes:
-        for node in nodes:
-            name = datetime.strptime(node.text, '%Y-%m-%d').date()
-    return name
-
-async def get_info(lang, title):
-    print('INFO\tget_info({}, {}'.format(lang, title))
-    text = get_text('https://{}.wikipedia.org/wiki/{}'.format(lang, title))
-    if text is None:
-        return {'name': title, 'countries': []}
-    category = {'name': title, 'countries': []}
-    if text:
-        soup = BeautifulSoup(text, 'html.parser')
-        category['countries'] = get_country_info(soup)
-        category['name'] = get_name_info(soup)
-        if category['name'] == None:
-            category['name'] = title
-        category['bday'] = get_bday_info(soup)
-    print('INFO\tget_info({}, {})\r\n\t{}'.format(lang, title, category))
-    return category
-
-
-
-async def get_links(lang, title):
-    text = get_text('https://{}.wikipedia.org/wiki/{}'.format(lang, title))
-    if text is None:
-        return {'title': title, 'links': []}
-    category = {'title': title, 'links': []}
-    if text:
-        soup = BeautifulSoup(text, 'html.parser')
-        cat_nodes = soup.select('div#mw-content-text a[title]')
-        if cat_nodes:
-            for cat_node in cat_nodes:
-                category['links'].append(cat_node.get('title'))
-    return category
+        wdi_nodes = soup.select('script.js-entity-header-select-data')
+        client.ibustats.seasons.update_one({'cc_id': 597}, {
+            '$set': {'title': '2020/2021'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 551}, {
+            '$set': {'title': '2019/2020'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 497}, {
+            '$set': {'title': '2018/2019'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 450}, {
+            '$set': {'title': '2017/2018'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 402}, {
+            '$set': {'title': '2016/2017'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 334}, {
+            '$set': {'title': '2015/2016'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 283}, {
+            '$set': {'title': '2014/2015'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 172}, {
+            '$set': {'title': '2013/2014'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 83}, {
+            '$set': {'title': '2012/2013'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 55}, {
+            '$set': {'title': '2011/2012'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 34}, {
+            '$set': {'title': '2010/2011'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 15}, {
+            '$set': {'title': '2009/2010'}}, upsert=True)
+        client.ibustats.seasons.update_one({'cc_id': 2}, {
+            '$set': {'title': '2008/2009'}}, upsert=True)
