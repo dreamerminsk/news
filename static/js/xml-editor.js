0f9aac6a8c33dfc8125d1c7b19f4bccf571bab68
Micah Teeuws <tutmirage@gmail.com>
2021-01-14 22:06:39 +0300

    Update xml-editor.js

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -32,7 +32,7 @@ async function loadFeed(url) {
 
 function loadingAlert(url) {
     return `
-    <div class="alert alert-in/fo alert-dismissible fade show" role="alert">
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
