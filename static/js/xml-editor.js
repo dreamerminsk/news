cd68ae35017af1f34ac3d8170baf4d537f029411
Micah Teeuws <tutmirage@gmail.com>
2021-01-14 10:54:46 +0300

    Update xml-editor.js

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -29,7 +29,7 @@ document.getElementById('messages').innerHTML =
        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
          <span aria-hidden="true">&times;</span>
        </button>
-       ${e}
+       <strong>${e.name}</strong></hr>${e.message}
      </div>`;
 }
 }
