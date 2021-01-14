bab1c8b13d993f27313ede22058aaff356cc3940
Micah Teeuws <tutmirage@gmail.com>
2021-01-14 10:50:31 +0300

    Update xml-editor.js

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -25,7 +25,10 @@ try {
   }
 } catch(e) {
 document.getElementById('messages').innerHTML = 
-    `<div class="alert alert-danger" role="alert">
+    `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
        ${e}
      </div>`;
 }
