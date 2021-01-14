74bf4671eb0414010dc469512cda199102ec2c95
Micah Teeuws <tutmirage@gmail.com>
2021-01-14 17:49:25 +0300

    Update xml-editor.js

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -30,6 +30,15 @@ async function loadFeed(url) {
     }
 }
 
+function loadingAlert(url) {
+    `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+       </button>
+       <strong>${e.name}</strong></hr>${e.message}
+     </div>`;
+}
+
 function errorAlert(e) {
     `<div class="alert alert-danger alert-dismissible fade show" role="alert">
        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
