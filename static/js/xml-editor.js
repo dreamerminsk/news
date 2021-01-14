8e5527c6c402c413d2bfea39a51e768b599532f1
Каролина Чурейно <kchureyno@outlook.com>
2021-01-14 14:57:16 +0300

    Update xml-editor.js

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -31,7 +31,6 @@ async function loadFeed(url) {
 }
 
 function errorAlert(e) {
-    return
     `<div class="alert alert-danger alert-dismissible fade show" role="alert">
        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
          <span aria-hidden="true">&times;</span>
