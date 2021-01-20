701bf64ed7826dc1af538a304fc6d79e9dd42384
Каролина Чурейно <kchureyno@outlook.com>
2021-01-20 11:17:14 +0300

    export const whitespaces

--- static/js/xml-editor.js
+++ static/js/xml-editor.js
@@ -3,9 +3,7 @@ let currentNode;
 let contentNode;
 let messagesNode;
 let listNodes;
-const whitespaces = new Set([9, 10, 11, 12, 13, 32, 133, 160, 5760,
-  8192, 8193, 8194, 8195, 8196, 8197, 8198, 8199, 8200, 8201,
-  8202, 8232, 8233, 8239, 8287, 12288]);
+
 
 let headers = new Headers({
   "User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:83.0) Gecko/20100101 Firefox/83.0"
