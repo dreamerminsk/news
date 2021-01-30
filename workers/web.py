f5d7f50bca12c02c2f0c81a50dbc1dbed4b23da3
Micah Teeuws <tutmirage@gmail.com>
2021-01-30 10:18:51 +0300

    Update web.py

--- workers/web.py
+++ workers/web.py
@@ -1,5 +1,6 @@
 import requests
 import httpx
+import asyncio
 
 from fake_useragent import UserAgent
 
@@ -7,6 +8,15 @@ from fake_useragent import UserAgent
 def get_text(url):
     try:
         r = requests.get(url, headers={'User-Agent': UserAgent().random})
+        return r.text
+    except Exception as e:
+        print('ERROR: {}\r\n{}'.format(url, e))
+        return None
+    
+async def get_text_async(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, headers={'User-Agent': UserAgent().random})
         return r.text
     except Exception as e:
         print('ERROR: {}\r\n{}'.format(url, e))
