ea6951ab4752613578765961e2474befec12cab8
Каролина Чурейно <kchureyno@outlook.com>
2021-02-02 23:02:05 +0300

    Update news.py

--- news.py
+++ news.py
@@ -21,7 +21,7 @@ from rels.humans import HumanEndpoint, HumansEndpoint
 from rels.instances import InstanceEndpoint, InstancesEndpoint
 from workers.championat import process_players, process_seasons
 from workers.web import get_text, get_text_async
-from workers.wiki import get_info
+from workers.wiki import get_info, process_countries
 
 #print = pprint.pprint
 
@@ -143,7 +143,8 @@ async def start_job():
     tasks = [loop.create_task(queue_feeds(q)),
              loop.create_task(process_feeds(q)),
              loop.create_task(queue_wiki_info()),
-             loop.create_task(process_players())]
+             loop.create_task(process_players()),
+             loop.create_task(process_countries())]
 
 
 async def queue_wiki_info():
