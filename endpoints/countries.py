e8ab37e57c8b98b6c566103bf24e741b2158713a
Micah Teeuws <tutmirage@gmail.com>
2021-02-02 18:56:58 +0300

    Update countries.py

--- endpoints/countries.py
+++ endpoints/countries.py
@@ -13,27 +13,25 @@ client = MongoClient()
 ibustats = client.ibustats
 
 
-class RacersEndpoint(HTTPEndpoint):
+class CountriesEndpoint(HTTPEndpoint):
     async def get(self, request):
-        racers = ibustats.racers.find({})
+        racers = ibustats.countries.find({})
         latest = []
         for racer in racers:
             racer['_id'] = str(racer['_id'])
-            if 'bday' in racer:
-                racer['bday'] = str(racer['bday'])
             if 'last_modified' in racer:
                 racer['last_modified'] = str(racer['last_modified'])
             latest.append(racer)
-        return JSONResponse({'status': 'ok', 'racers': latest})
+        return JSONResponse({'status': 'ok', 'countries': latest})
 
     async def post(self, request):
         pass
 
 
-class RacerEndpoint(HTTPEndpoint):
+class CountryEndpoint(HTTPEndpoint):
     async def get(self, request):
         wdid = request.path_params['wikidataid']
-        task = ibustats.racers.find_one({'WikiDataID': wdid})
+        task = ibustats.countries.find_one({'WikiDataID': wdid})
         if task is not None:
             task['_id'] = str(task['_id'])
         else:
@@ -42,23 +40,4 @@ class RacerEndpoint(HTTPEndpoint):
         return JSONResponse(task)
 
     async def put(self, request):
-        pass
-
-
-class NamesEndpoint(HTTPEndpoint):
-    async def get(self, request):
-        startswith = request.path_params['startswith']
-        racers = ibustats.racers.find(
-            {'wiki.ru': {'$regex': '^{}'.format(startswith)}})
-        latest = []
-        for racer in racers:
-            racer['_id'] = str(racer['_id'])
-            if 'bday' in racer:
-                racer['bday'] = str(racer['bday'])
-            if 'last_modified' in racer:
-                racer['last_modified'] = str(racer['last_modified'])
-            latest.append(racer)
-        return JSONResponse({'status': 'ok', 'racers': latest})
-
-    async def post(self, request):
         pass
