17f84b1a47b64f72fe9a2eaf787df5b4c873f702
Каролина Чурейно <kchureyno@outlook.com>
2021-02-02 20:43:18 +0300

    Update racers.js

--- static/js/racers.js
+++ static/js/racers.js
@@ -119,12 +119,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 
 
-function getAge( birthDate ) {
-    let today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    let m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
+function getAge(birthDate) {
+  let today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
