4fdbede3d93fae107b19bf8921370253f2608098
Каролина Чурейно <kchureyno@outlook.com>
2021-02-03 13:37:56 +0300

    Update countries.js

--- static/js/countries.js
+++ static/js/countries.js
@@ -20,10 +20,10 @@ async function filter() {
          <div class="card-header">${country.wiki.ru}</div>
          <div class="card-body text-dark">
 		   <div class="row">
-		     <div class="col-sm-6 col-md-6 col-lg-3">
-			   <img class="card-img-top" src="${country.flag}">
+		     <div class="col-xs-6 col-sm-6 col-md-5 col-lg-3">
+			   <img class="img-fluid" src="${country.flag}">
 			 </div>
-			 <div class="col-sm-6 col-md-6 col-lg-9">
+			 <div class="col-xs-6 col-sm-6 col-md-7 col-lg-9">
 			   <h6 class="card-title">${country.wiki.ru}</h6>
 			 </div>
 		   </div>
