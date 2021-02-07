41cea44a8ede7328e4f9d1c28d7d47e9e2966790
Micah Teeuws <tutmirage@gmail.com>
2021-02-08 00:17:33 +0300

    Update Birthdates.js

--- static/js/Birthdates.js
+++ static/js/Birthdates.js
@@ -11,7 +11,8 @@ async function init() {
     let button = event.target.closest('button');
     if (!button) return;
     if (!row.contains(button)) return;
-    document.querySelector('#birthdates').textContent = button.textContent;
+    document.querySelector('#birthdates').textContent =
+      (Number(document.querySelector('.dropdown-toggle').textContent) + Number(button.textContent)).toString();
   };
 }
 
