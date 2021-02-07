bf55ebef667d840ec97faebf208006e4b3c1f3aa
Каролина Чурейно <kchureyno@outlook.com>
2021-02-07 10:01:22 +0300

    m-0 p-0

--- static/js/countries.js
+++ static/js/countries.js
@@ -27,21 +27,21 @@ async function filter() {
 			   <img class="img-fluid rounded shadow-sm" src="${country?.emblem ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Herb_%C5%81ab%C4%99d%C5%BA_1.svg/1024px-Herb_%C5%81ab%C4%99d%C5%BA_1.svg.png'}">
              </div>
              <div class="m-0 p-0 col-6 col-sm-6 col-md-7 col-lg-9">
-               <p class="card-text">${country?.wiki?.ru}</p>
-               <p class="card-text">${country?.pvi_month?.ru}</p>
-               <small class="text-muted">${country?.lasttime?.ru}</small>
+               <p class="m-0 p-0 card-text">${country?.wiki?.ru}</p>
+               <p class="m-0 p-0 card-text">${country?.pvi_month?.ru}</p>
+               <small class="m-0 p-0 text-muted">${country?.lasttime?.ru}</small>
                <hr/>
-               <p class="card-text">${country?.wiki?.en}</p>
-               <p class="card-text">${country?.pvi_month?.en}</p>
-               <small class="text-muted">${country?.lasttime?.en}</small>
+               <p class="m-0 p-0 card-text">${country?.wiki?.en}</p>
+               <p class="m-0 p-0 card-text">${country?.pvi_month?.en}</p>
+               <small class="m-0 p-0 text-muted">${country?.lasttime?.en}</small>
                <hr/>
-               <p class="card-text">${country?.wiki?.de}</p>
-               <p class="card-text">${country?.pvi_month?.de}</p>
-               <small class="text-muted">${country?.lasttime?.de}</small>
+               <p class="m-0 p-0 card-text">${country?.wiki?.de}</p>
+               <p class="m-0 p-0 card-text">${country?.pvi_month?.de}</p>
+               <small class="m-0 p-0 text-muted">${country?.lasttime?.de}</small>
                <hr/>
-               <p class="card-text">${country?.wiki?.fr}</p>
-               <p class="card-text">${country?.pvi_month?.fr}</p>
-               <small class="text-muted">${country?.lasttime?.fr}</small>
+               <p class="m-0 p-0 card-text">${country?.wiki?.fr}</p>
+               <p class="m-0 p-0 card-text">${country?.pvi_month?.fr}</p>
+               <small class="m-0 p-0 text-muted">${country?.lasttime?.fr}</small>
              </div>
            </div>
          </div>
