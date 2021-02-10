799f3acbe3f24ead56b0d2bdde864b89976e545f
Каролина Чурейно <kchureyno@outlook.com>
2021-02-10 22:25:03 +0300

    Update admin.py

---
+++ endpoints/admin.py
@@ -0,0 +1,19 @@
+import asyncio
+import pprint
+from datetime import datetime, timedelta
+
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import (JSONResponse, PlainTextResponse,
+                                 RedirectResponse)
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='templates')
+
+client = MongoClient()
+
+
+class AdminView(HTTPEndpoint):
+    async def get(self, request):
+        return templates.TemplateResponse('Birthdates.html', {'request': request})
