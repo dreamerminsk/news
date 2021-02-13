58694003c5cb1d3b15fd56790a4252c928ae4c4f
Micah Teeuws <tutmirage@gmail.com>
2021-02-13 13:33:10 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -35,28 +35,12 @@ let yearProxy = new Proxy(selected, {
 
 async function init() {
   let row = document.querySelector('.dropdown-menu');
-  row.onclick = function (event) {
-    let button = event.target.closest('button');
-    if (!button) return;
-    if (!row.contains(button)) return;
-    yearProxy.decade = Number(button.textContent);
-  };
-  let row2 = document.querySelector('#years');
-  row2.onclick = function (event) {
-    let button = event.target.closest('button');
-    if (!button) return;
-    if (!row2.contains(button)) return;
-    yearProxy.year = Number(button.textContent);
-  };
+  let url = `/api/admin/dbs`;
+  let page = await fetch(url);
+  let json = await page.json();
 }
 
 
 document.addEventListener('DOMContentLoaded', function (event) {
   init();
-  let fullYear = localStorage.getItem('fullYear');
-  if (fullYear) {
-    yearProxy.fullYear = fullYear;
-  } else {
-    yearProxy.fullYear = 1998;
-  }
 });
