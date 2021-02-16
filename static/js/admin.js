cf4a6f4e91fdc857b6c628ac6905510c1c5066a6
Micah Teeuws <tutmirage@gmail.com>
2021-02-16 14:19:11 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -40,7 +40,28 @@ async function init() {
   let json = await page.json();
   let t = ``;
   for (let db of json.dbs) {
-    t += `<div class="card"><div class="card-body">${db.name}</th><td>${db.sizeOnDisk}</div></div>`;
+    t += `
+    <div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">${db.name}</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">${db.sizeOnDisk}</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    `;
   }
   t += ``;
   row.innerHTML += t;
