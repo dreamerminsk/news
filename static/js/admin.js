d7460a985273d929f12e35dfa90fe7f383f6cca3
Micah Teeuws <tutmirage@gmail.com>
2021-02-17 10:29:41 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,38 +1,3 @@
-let selected = { 'year': 2000 };
-
-let yearProxy = new Proxy(selected, {
-  set(target, property, value, receiver) {
-    let success = false;
-    if (property === 'fullYear') {
-      success = Reflect.set(target, 'year', value, receiver);
-      if (success) {
-        localStorage.setItem('fullYear', target.year);
-        decade(value - (value % 10));
-        year(value % 10);
-      }
-    }
-    if (property === 'decade') {
-      success = Reflect.set(target, 'year', value + (target.year % 10), receiver);
-      if (success) {
-        localStorage.setItem('fullYear', target.year);
-        decade(value);
-        year(target.year % 10);
-      }
-    }
-    if (property === 'year') {
-      success = Reflect.set(target, 'year', target.year - (target.year % 10) + value, receiver);
-      if (success) {
-        localStorage.setItem('fullYear', target.year);
-        year(value);
-      }
-    }
-    return success;
-  }
-});
-
-
-
-
 async function init() {
   let row = document.querySelector('#content');
   let url = `/api/admin/dbs`;
