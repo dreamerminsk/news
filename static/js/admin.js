a57568b24c1eb79b8b615619c2c2b9ce6a72195f
Micah Teeuws <tutmirage@gmail.com>
2021-02-17 13:03:23 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -7,29 +7,31 @@ async function init() {
   for (let [index, db] of json.dbs.entries()) {
     t += `
     <div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-pills card-header-pills" id="card-header-pills-${index}" role="tablist">
-      <li class="nav-item" role="presentation">
-        <a class="nav-link active" data-bs-toggle="pill" id="home-tab-${index}" href="#home-${index}" role="tab" aria-controls="home" aria-selected="true">${db.name}</a>
-      </li>
-      <li class="nav-item" role="presentation">
-        <a class="nav-link" data-bs-toggle="pill" id="profile-tab-${index}" href="#profile-${index}" role="tab" aria-controls="profile" aria-selected="false">profile</a>
-      </li>
-      <li class="nav-item" role="presentation">
-        <a class="nav-link data-bs-toggle="pill" id="messages-tab-${index}" href="#messages" role="tab" aria-controls="messages" aria-selected="false">messages</a>
-      </li>
-    </ul>
-  </div>
+      <div class="card-header">
+        <ul class="nav nav-pills card-header-pills" id="card-header-pills-${index}" role="tablist">
+          <li class="nav-item" role="presentation">
+            <a class="nav-link active" data-bs-toggle="pill" id="home-tab-${index}" href="#home-${index}" role="tab" aria-controls="home" aria-selected="true">${db.name}</a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a class="nav-link" data-bs-toggle="pill" id="profile-tab-${index}" href="#profile-${index}" role="tab" aria-controls="profile" aria-selected="false">profile</a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a class="nav-link data-bs-toggle="pill" id="messages-tab-${index}" href="#messages-${index}" role="tab" aria-controls="messages" aria-selected="false">messages</a>
+          </li>
+        </ul>
+      </div>
 
-  <div class="card-body">
-    <div class="tab-content">
-  <div class="tab-pane  fade show active" id="home-${index}" role="tabpanel" aria-labelledby="home-tab">home-${index}</div>
-  <div class="tab-pane  fade show" id="profile-${index}" role="tabpanel" aria-labelledby="profile-tab">profile-${index}</div>
-  <div class="tab-pane  fade show" id="messages-${index}" role="tabpanel" aria-labelledby="messages-tab">...</div>
-  <div class="tab-pane  fade show" id="settings-${index}" role="tabpanel" aria-labelledby="settings-tab">...</div>
-</div>
-  </div>
-</div>
+      <div class="card-body">
+        <div class="tab-content">
+          <div class="tab-pane  fade show active" id="home-${index}" role="tabpanel" aria-labelledby="home-tab">
+            home-${index}
+          </div>
+          <div class="tab-pane  fade show" id="profile-${index}" role="tabpanel" aria-labelledby="profile-tab">profile-${index}</div>
+          <div class="tab-pane  fade show" id="messages-${index}" role="tabpanel" aria-labelledby="messages-tab">messages-${index}</div>
+          <div class="tab-pane  fade show" id="settings-${index}" role="tabpanel" aria-labelledby="settings-tab">...</div>
+        </div>
+      </div>
+    </div>
     `;
     setInterval(function() {
     var triggerTabList = [].slice.call(document.querySelectorAll(`#card-header-pills-${index} a`));
