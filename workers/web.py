491cf460a996a92811d6a6734f8a3893107dae02
Каролина Чурейно <kchureyno@outlook.com>
2021-02-21 11:49:06 +0300

    Update web.py

--- workers/web.py
+++ workers/web.py
@@ -2,6 +2,7 @@ import asyncio
 
 import httpx
 import requests
+from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
@@ -19,5 +20,15 @@ async def get_text_async(url):
         async with httpx.AsyncClient() as client:
             r = await client.get(url, headers={'User-Agent': UserAgent().random})
         return (r.text, None)
+    except Exception as e:
+        return (None, e)
+
+
+async def get_html_async(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, headers={'User-Agent': UserAgent().random})
+            html = BeautifulSoup(r.text, 'html.parser')
+        return (html, None)
     except Exception as e:
         return (None, e)
