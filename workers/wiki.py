8ac9042a0313b22ead12ff498f0517cee9ff17a1
Каролина Чурейно <kchureyno@outlook.com>
2021-02-24 13:57:23 +0300

    Update wiki.py

--- workers/wiki.py
+++ workers/wiki.py
@@ -56,10 +56,10 @@ class Article(object):
         return 'https://{}.wikipedia.org/wiki/{}'.format(self.lang, self.title)
         
     @property
-    def param(self):
-        if not hasattr(self, '_param'):
-            self._param = __parse(self)
-        return self._param
+    async def categories(self):
+        if not hasattr(self, '__categories'):
+            await __parse(self)
+        return self.__categories
 
     async def __parse(self):
         html, error = await get_html_async(self.url)
