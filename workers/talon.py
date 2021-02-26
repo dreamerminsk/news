fbb9d4a3706cd0b7f9e0c9669f3aa1e16b7c2268
Каролина Чурейно <kchureyno@outlook.com>
2021-02-26 13:24:41 +0300

    Update talon.py

--- workers/talon.py
+++ workers/talon.py
@@ -11,3 +11,6 @@ talon = 'https://talon.by/'
 p = 'https://talon.by/policlinic/minsk-1dp/order'
 d = 'https://talon.by/policlinic/minsk-1dp/order/1943'
 t = 'https://talon.by/policlinic/minsk-1dp/order/1943/3473'
+
+dd = 'https://talon.by/policlinic/minsk-1dp/doctors/16484'
+ddt = 'https://talon.by/policlinic/minsk-1dp/order/1944/3483'
