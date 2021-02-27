0c6ecfd6553812a07eda9de36646ad651866bf75
Каролина Чурейно <kchureyno@outlook.com>
2021-02-27 20:19:32 +0300

    Update talon.py

--- workers/talon.py
+++ workers/talon.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import requests
-from bs4 import BeautifulSoup
 import time
 
-
+import requests
+from bs4 import BeautifulSoup
 
 talon = 'https://talon.by/'
 
@@ -15,8 +14,6 @@ t = 'https://talon.by/policlinic/minsk-1dp/order/1943/3473'
 dd = 'https://talon.by/policlinic/minsk-1dp/doctors/16484'
 ddt = 'https://talon.by/policlinic/minsk-1dp/order/1944/3483'
 ddtt = 'https://talon.by/policlinic/minsk-1dp/order/1944/3483/2125898'
-
-
 
 
 policlinics = 'https://talon.by/policlinics'
