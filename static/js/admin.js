c5e3228f281b2f8d941a83ee040dfaa5a983a472
Каролина Чурейно <kchureyno@outlook.com>
2021-02-27 20:06:23 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -33,17 +33,17 @@ async function init() {
       </div>
     </div>
     `;
-    setInterval(function() {
-    var triggerTabList = [].slice.call(document.querySelectorAll(`#card-header-pills-${index} a`));
-    triggerTabList.forEach(function (triggerEl) {
-      var tabTrigger = new bootstrap.Tab(triggerEl);
+    setInterval(function () {
+      var triggerTabList = [].slice.call(document.querySelectorAll(`#card-header-pills-${index} a`));
+      triggerTabList.forEach(function (triggerEl) {
+        var tabTrigger = new bootstrap.Tab(triggerEl);
 
-      triggerEl.addEventListener('click', function (event) {
-        event.preventDefault();
-        tabTrigger.show();
+        triggerEl.addEventListener('click', function (event) {
+          event.preventDefault();
+          tabTrigger.show();
+        });
       });
-    });
-  }, 100);
+    }, 100);
   }
   row.innerHTML += t;
 }
