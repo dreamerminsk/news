13fea4f53707f03ecb2ac09b986846f6ea97a92c
Micah Teeuws <tutmirage@gmail.com>
2021-02-27 19:18:41 +0300

    Update wiki.py
    Remove get_category

--- workers/wiki.py
+++ workers/wiki.py
@@ -87,23 +87,6 @@ class Article(object):
                 self.__interwikis.append(Article(node.get('lang'), lang_title))
 
 
-async def get_category(title):
-    text = get_text('https://en.wikipedia.org/wiki/{}'.format(title))
-    if text is None:
-        return {'wikidataid': None, 'categories': []}
-    category = {'wikidataid': None, 'categories': []}
-    if text:
-        soup = BeautifulSoup(text, 'html.parser')
-        wdi_node = soup.select_one('li#t-wikibase a[href]')
-        if wdi_node:
-            category['wikidataid'] = wdi_node.get('href').split('/')[-1]
-        cat_nodes = soup.select('div#mw-normal-catlinks ul li a[title]')
-        if cat_nodes:
-            for cat_node in cat_nodes:
-                category['categories'].append(cat_node.get('title'))
-    return category
-
-
 def get_country_info(soup):
     countries = []
     nodes = soup.select("span[data-wikidata-property-id='P27'] a[title]")
