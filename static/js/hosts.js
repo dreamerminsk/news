dd42917e84cadd47afaece88d8cd7ea06f81db95
Micah Teeuws <tutmirage@gmail.com>
2021-03-04 00:25:48 +0300

    Update hosts.js

--- static/js/hosts.js
+++ static/js/hosts.js
@@ -3,5 +3,8 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  document.querySelectorAll('');
+  let hs = document.querySelectorAll('.card-header');
+  hs.forEach(function(h, index){
+    
+  });
 });
