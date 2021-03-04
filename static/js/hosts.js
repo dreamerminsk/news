eada44d4921a42d32d15ca702a2c9e24b4c6617d
Каролина Чурейно <kchureyno@outlook.com>
2021-03-04 14:37:52 +0300

    Update hosts.js

--- static/js/hosts.js
+++ static/js/hosts.js
@@ -4,7 +4,7 @@ function init(header) {
 
 document.addEventListener('DOMContentLoaded', function (event) {
   let hs = document.querySelectorAll('.card-header');
-  hs.forEach(function(h, index){
-    
+  hs.forEach(function (h, index) {
+
   });
 });
