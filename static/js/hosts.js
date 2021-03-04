a89004c838867e74517e9c9abfe87fd72601f252
Micah Teeuws <tutmirage@gmail.com>
2021-03-04 17:42:52 +0300

    Update hosts.js

--- static/js/hosts.js
+++ static/js/hosts.js
@@ -5,18 +5,22 @@ async function init(header) {
     let json = await r.json();
     header.textContent += ` - ${json.country} - ${json.timezone}`;
     parent = header.parentNode;
-    parent.classList.add('border-primary')
-    parent.classList.remove('border-dark')
+    parent.classList.add('border-primary');
+    parent.classList.remove('border-dark');
+    parent.classList.add('card-primary');
+    parent.classList.remove('card-dark');
   } else {
     parent = header.parentNode;
     parent.classList.add('border-danger');
     parent.classList.remove('border-dark');
+    parent.classList.add('card-danger');
+    parent.classList.remove('card-dark');
   }
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
   let hs = document.querySelectorAll('.card-header');
   hs.forEach(function (h, index) {
-    setTimeout(init, Math.floor(Math.random() * 2000) + 2000, h);
+    setTimeout(init, Math.floor(Math.random() * 3000) + 2000, h);
   });
 });
