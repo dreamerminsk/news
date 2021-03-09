5a179cf1abd54ad94c72384f82b97d207e2e1d68
Micah Teeuws <tutmirage@gmail.com>
2021-03-09 12:48:16 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -58,7 +58,7 @@ async function init() {
     t += `
     <div class="card text-white bg-dark mb-3 text-center">
       <div class="card-body">
-        <h6 class="card-title">${db.name}</p>
+        <h5 class="card-title">${db.name}</h5>
         <p class="card-text">${db.sizeOnDisk}</p>
       </div>
     </div>
