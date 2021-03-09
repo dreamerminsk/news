a0fd82712b4d5f277d44fb996a0c883a907a6184
Micah Teeuws <tutmirage@gmail.com>
2021-03-09 20:21:08 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -46,6 +46,18 @@ async function init2() {
     }, 100);
   }
   row.innerHTML += t;
+  row.innerHTML += '<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Checked checkbox
+  </label>
+</div>';
 }
 
 async function init() {
