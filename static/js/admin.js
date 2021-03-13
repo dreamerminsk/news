ce5ca5ab25d3b492f6af5708d45bd40bb568a9c0
Каролина Чурейно <kchureyno@outlook.com>
2021-03-13 12:48:42 +0300

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,11 +1,11 @@
 async function init2() {
-    let row = document.querySelector('#content');
-    let url = `/api/admin/dbs`;
-    let page = await fetch(url);
-    let json = await page.json();
-    let t = ``;
-    for (let [index, db] of json.dbs.entries()) {
-        t += `
+  let row = document.querySelector('#content');
+  let url = `/api/admin/dbs`;
+  let page = await fetch(url);
+  let json = await page.json();
+  let t = ``;
+  for (let [index, db] of json.dbs.entries()) {
+    t += `
     <div class="card text-center">
       <div class="card-header">
         <ul class="nav nav-pills card-header-pills" id="card-header-pills-${index}" role="tablist">
@@ -33,29 +33,31 @@ async function init2() {
       </div>
     </div>
     `;
-        setInterval(function() {
-            var triggerTabList = [].slice.call(document.querySelectorAll(`#card-header-pills-${index} a`));
-            triggerTabList.forEach(function(triggerEl) {
-                var tabTrigger = new bootstrap.Tab(triggerEl);
+    setInterval(function () {
+      var triggerTabList = [].slice.call(document.querySelectorAll(`#card-header-pills-${index} a`));
+      triggerTabList.forEach(function (triggerEl) {
+        var tabTrigger = new bootstrap.Tab(triggerEl);
 
-                triggerEl.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    tabTrigger.show();
-                });
-            });
-        }, 100);
-    }
-    row.innerHTML += t;
+        triggerEl.addEventListener('click', function (event) {
+          event.preventDefault();
+          tabTrigger.show();
+        });
+      });
+    }, 100);
+  }
+  row.innerHTML += t;
 }
 
+
+
 async function dbs() {
-    let row = document.querySelector('#content');
-    let url = `/api/admin/dbs`;
-    let page = await fetch(url);
-    let json = await page.json();
-    let t = ``;
-    for (let [index, db] of json.dbs.entries()) {
-        t += `
+  let row = document.querySelector('#content');
+  let url = `/api/admin/dbs`;
+  let page = await fetch(url);
+  let json = await page.json();
+  let t = ``;
+  for (let [index, db] of json.dbs.entries()) {
+    t += `
     <div class="card text-dark bg-light m-2 text-center">
       <div class="card-body">
         <h5 class="card-title"><a class="link-dark stretched-link" href="/admin/dbs/${db.name}">${db.name}</a></h5>
@@ -63,65 +65,69 @@ async function dbs() {
       </div>
     </div>
     `;
-    }
-    row.innerHTML += t;
+  }
+  row.innerHTML += t;
 }
 
 
 
 async function db(name) {
-    let row = document.querySelector('#content');
-	let url = `/api/admin/dbs/${name}`;
-    let page = await fetch(url);
-    let json = await page.json();
-    let t = ``;
-    t += `
+  let row = document.querySelector('#content');
+  let url = `/api/admin/dbs/${name}`;
+  let page = await fetch(url);
+  let json = await page.json();
+  let t = ``;
+  t += `
     <div class="card text-dark bg-light m-2 text-center">
       <div class="card-body">
         <h5 class="card-title"><a class="link-dark stretched-link" href="/admin/dbs/${name}">${name}</a></h5>
       </div>
     </div>
     `;
-    row.innerHTML = t;
+  row.innerHTML = t;
 }
 
 
 
 async function router() {
-    if (window.location.pathname === '/admin/dbs') {
-        await dbs();
-    }
-    if (window.location.pathname.startsWith('/admin/dbs/')) {
-        let name = window.location.pathname.replace('/admin/dbs/', '')
-        await db(name);
-    }
+  if (window.location.pathname === '/admin/dbs') {
+    await dbs();
+  }
+  if (window.location.pathname.startsWith('/admin/dbs/')) {
+    let name = window.location.pathname.replace('/admin/dbs/', '')
+    await db(name);
+  }
 }
 
 
+
 function findLink(el) {
-    if (el.tagName == 'A' && el.href) {
-        return el.href;
-    } else if (el.parentElement) {
-        return findLink(el.parentElement);
-    } else {
-        return null;
-    }
+  if (el.tagName == 'A' && el.href) {
+    return el.href;
+  } else if (el.parentElement) {
+    return findLink(el.parentElement);
+  } else {
+    return null;
+  }
 };
+
+
 
 function handleClick(e) {
-    const link = findLink(e.target);
-    if (link == null) {
-        return;
-    }
-    e.preventDefault();
-    history.pushState({'url': link}, '', link);
-    router();
+  const link = findLink(e.target);
+  if (link == null) {
+    return;
+  }
+  e.preventDefault();
+  history.pushState({ 'url': link }, '', link);
+  router();
 };
 
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    window.addEventListener('popstate', () => router());
-    document.addEventListener('click', handleClick);
-    history.replaceState({'url': '/admin/dbs'}, '', '/admin/dbs')
-    router();
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('popstate', () => router());
+  document.addEventListener('click', handleClick);
+  history.replaceState({ 'url': '/admin/dbs' }, '', '/admin/dbs')
+  router();
 });
