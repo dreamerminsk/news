8fd0ae62eef0d36f122bd6f8fa1bc8c4d69e0897
Micah Teeuws <tutmirage@gmail.com>
2021-04-14 19:40:59 +0300

    Create onliner.js

---
+++ static/js/onliner.js
@@ -0,0 +1,46 @@
+async function dbs() {
+  let row = document.querySelector('#content');
+  let t = ``;
+  row.innerHTML = t;
+}
+
+
+
+async function router() {
+  if (window.location.pathname === '/onliner/') {
+    await dbs();
+  }
+}
+
+
+
+function findLink(el) {
+  if (el.tagName == 'A' && el.href) {
+    return el.href;
+  } else if (el.parentElement) {
+    return findLink(el.parentElement);
+  } else {
+    return null;
+  }
+};
+
+
+
+function handleClick(e) {
+  const link = findLink(e.target);
+  if (link == null) {
+    return;
+  }
+  e.preventDefault();
+  history.pushState({ 'url': link }, '', link);
+  router();
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('popstate', () => router());
+  document.addEventListener('click', handleClick);
+  history.replaceState({ 'url': '/onliner/' }, '', '/onliner')
+  router();
+});
